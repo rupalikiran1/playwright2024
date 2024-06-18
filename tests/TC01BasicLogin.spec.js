a24246a0f6f72387d6b870527f654fd8872f5065
rupalikiran1 <rupalikiran1999@gmail.com>
2024-06-18 23:40:25 +0530

    first commit

---
+++ tests/TC01BasicLogin.spec.js
@@ -0,0 +1,35 @@
+const {test,expect} = require('@playwright/test')
+
+test('verify login functionality with valid credentails',async({page})=>{
+
+    let dashboardEle = await page.locator('h6[class="oxd-test oxd-test--h6 oxd-topbar-header-breadcrumb-module"]')
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    await page.locator('input[name = "username"]').fill('Admin')
+
+    await page.locator('input[name = "password"]').fill('admin123')
+
+    await page.locator('button[type="submit"]').click()
+
+    await expect(dashboardEle).toBeVisible()
+    await expect(dashboardEle).toHaveText('Dashboard')
+    await expect(page.locator('img[alt="client brand banner"]')).toBeVisible()
+    await expect(page).toHaveTitle('OrangeHRM')
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+})
+
+test.only('verify login functionality with invalid credentails',async({page})=>{
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    await page.locator('input[name="username"]').fill('admin')
+
+    await page.locator('input[placeholder="passwprd"]').fill('pass123')
+
+    await page.locator('button[type="submit"]').click()
+
+    await expect(page.locator('div[role="alert"]')).toBeVisible()
+    await expect(page,locator('p[class="oxd-text oxd-text--p oxd-alert-content-text"]')).toHaveText('Invalid credentials')
+    
+})
