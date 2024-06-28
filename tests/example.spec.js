4f82000a7d916638c778bcf9f2ffe21f9635187e
rupalikiran1 <rupalikiran1999@gmail.com>
2024-06-28 15:16:47 +0530

    first commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,15 +1,15 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test')
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/')
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Playwright/)
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/')
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
